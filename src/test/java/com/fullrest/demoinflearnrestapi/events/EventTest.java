0e7086d8e4d73442506e976e5cb4fb3bd48f67b6
HyunjunJeon <jeonhj920@gmail.com>
2019-05-12 22:44:45 +0900

    Test Code Refactoring - JunitParams Lib를 이용해서 파라미터를 처리하고, String[] 형태로 입력하는 것을 따로 메서드를 만들어주어 Type Safe 하게 Input 시키는 것 까지

--- src/test/java/com/fullrest/demoinflearnrestapi/events/EventTest.java
+++ src/test/java/com/fullrest/demoinflearnrestapi/events/EventTest.java
@@ -1,9 +1,13 @@
 package com.fullrest.demoinflearnrestapi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
     @Test
     public void builder(){
@@ -30,63 +34,51 @@ public class EventTest {
         assertThat(event.getDescription()).isEqualTo(des);
     }
 
-    @Test
-    public void testFree(){
-        // Given
-        Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
-                .build();
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isTrue();
-
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-
-        // Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
+    // Parameters Type Safety 하게 변경함
+    private Object[] parametersForTestFree(){
+        return new Object[]{
+            new Object[]{0,0,true},
+            new Object[]{100,0,false},
+            new Object[]{0,100,false },
+            new Object[]{100,200,false}
+        };
     }
 
     @Test
-    public void testOffline(){
+    //@Parameters({"0,0,true", "100,0,false", "0,100,false"})
+    //@Parameters(method = "parametersForTestFree")
+    @Parameters // Method Naming Convention 지켰을 때 (parametersFor~~)
+    public void testFree(int basePrice, int maxPrice, boolean isFree){
         // Given
         Event event = Event.builder()
-                .location("장소가 존재하는 중")
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
+    private Object[] parametersForTestOffline(){
+        return new Object[]{
+                new Object[]{"장소가 존재하는 중~", true},
+                new Object[]{null, false}
+        };
+    }
+
+    @Test
+    @Parameters
+    public void testOffline(String location, boolean isOffline){
         // Given
-        event = Event.builder().build();
-
+        Event event = Event.builder()
+                .location(location)
+                .build();
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
     }
 }
